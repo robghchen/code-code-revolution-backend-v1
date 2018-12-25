65086d3f4b1ddc08eda7f02e1a3001dc632b5670
github username <github email address>
2018-12-24 21:21:50 -0500

    removed highscore attribute

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2018_12_24_193816) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "highscore"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
